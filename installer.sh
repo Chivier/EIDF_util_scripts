6080bac25305e4a4927ad86ba3cac122efd28a1b
Chivier Humber <chivier.humber@outlook.com>
2023-10-31 18:43:16 +0800

    update: install nvitop

---
+++ installer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if command -v pip3 &>/dev/null; then
+	PIP_COMMAND=pip3
+elif command -v pip &>/dev/null; then
+	PIP_COMMAND=pip
+else
+	echo "Neither pip nor pip3 is installed"
+	exit 1
+fi
+
+echo "Using $PIP_COMMAND"
+
+$PIP_COMMAND install nvitop
