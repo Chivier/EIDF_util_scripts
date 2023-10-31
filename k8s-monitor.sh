0b52cfd81861e2f38a005031dbbbf02eea715791
Chivier Humber <chivier.humber@outlook.com>
2023-10-31 14:36:08 +0000

    update: auto summary

--- k8s-monitor.sh
+++ k8s-monitor.sh
@@ -2,16 +2,17 @@
 
 FILENAME=log.txt
 TAG=log
-INTERVAL=1
-DURATION=10
+INTERVAL=5
+DURATION=300
 
+# Copy new script
 while IFS= read -r podname
 do
     echo "$podname"
     kubectl cp collect.py $podname:/tmp
 done < "pod-list.txt"
 
-
+# Collect metrics
 while IFS= read -r podname
 do
     echo "$podname"
@@ -20,10 +21,17 @@ do
 done < "pod-list.txt"
 sleep $DURATION
 
+# Copy log to head
 while IFS= read -r podname
 do
     echo "Copy back from $podname..."
     kubectl cp $podname:/tmp/log.txt $podname-log.txt
 done < "pod-list.txt"
 
-
+# Generate summary
+while IFS= read -r podname
+do
+    echo "$podname"
+    python3 summary.py $podname-log.txt $TAG $INTERVAL $DURATION > $podname-summary.json
+done < "pod-list.txt"
+sleep $DURATION
