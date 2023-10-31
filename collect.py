7195a28c7aab2ec09edf0e191ad62c90c5575a93
Chivier Humber <chivier.humber@outlook.com>
2023-10-31 18:43:04 +0800

    update: basic collector:

---
+++ collect.py
@@ -0,0 +1,40 @@
+from nvitop import ResourceMetricCollector, Device, collect_in_background
+import sys
+import os
+import time
+import logging
+import threading
+
+# Configure the logger to write to a file
+
+def print_log(filename, collector_str):
+    with open(filename, "a") as file:
+        file.write(collector_str + "\n")
+
+if len(sys.argv) != 5:
+    print("Usage: python3 collect.py filename tagname interval duration")
+    exit(1)
+    
+filename = sys.argv[1]
+tagname = sys.argv[2]
+interval = float(sys.argv[3])
+duartion = float(sys.argv[4])
+
+start_time = time.time()
+
+def run():
+    collector = ResourceMetricCollector(devices=Device.cuda.all())
+    with collector(tag=tagname):
+        if time.time() - start_time < duartion:
+            threading.Timer(interval, run).start()  # schedule the function to run
+            collector_metric = collector.collect()
+            collector_str = str(collector_metric)
+            print_log(filename, collector_str)
+        else:
+            return
+    
+# if exists, delete the file
+if os.path.exists(filename):
+    os.remove(filename)
+    
+run()
